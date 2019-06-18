69e53f4a43b2f8e123cc2d35e7c61609b49d6a3c
lzybluee <lzybluee@gmail.com>
2019-06-18 10:02:14 +0800

    Add missing cards

---
+++ Mage.Sets/src/mage/cards/t/ThroneOfGeth.java
@@ -0,0 +1,48 @@
+package mage.cards.t;
+
+import mage.abilities.Ability;
+import mage.abilities.common.SimpleActivatedAbility;
+import mage.abilities.costs.common.SacrificeTargetCost;
+import mage.abilities.costs.common.TapSourceCost;
+import mage.abilities.effects.common.counter.ProliferateEffect;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.Zone;
+import mage.filter.common.FilterControlledPermanent;
+import mage.filter.predicate.mageobject.CardTypePredicate;
+import mage.target.common.TargetControlledPermanent;
+
+import java.util.UUID;
+
+/**
+ * @author Loki
+ */
+public final class ThroneOfGeth extends CardImpl {
+
+    private static final FilterControlledPermanent filter = new FilterControlledPermanent("an artifact");
+
+    static {
+        filter.add(new CardTypePredicate(CardType.ARTIFACT));
+    }
+
+    public ThroneOfGeth(UUID ownerId, CardSetInfo setInfo) {
+
+        super(ownerId, setInfo, new CardType[]{CardType.ARTIFACT}, "{2}");
+
+        // {T}, Sacrifice an artifact: Proliferate. (You choose any number of permanents and/or players with counters on them, then give each another counter of a kind already there.)
+        Ability ability = new SimpleActivatedAbility(Zone.BATTLEFIELD, new ProliferateEffect(), new TapSourceCost());
+        ability.addCost(new SacrificeTargetCost(new TargetControlledPermanent(filter)));
+        this.addAbility(ability);
+    }
+
+    public ThroneOfGeth(final ThroneOfGeth card) {
+        super(card);
+    }
+
+    @Override
+    public ThroneOfGeth copy() {
+        return new ThroneOfGeth(this);
+    }
+
+}
